2995de955495954f830569dfe84e20c5d8e4faed
taylorkmho <taylorkmho@gmail.com>
2017-11-13 07:24:32 -0800

    preventing bug where video caption goes hidden during edit

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -4,7 +4,7 @@ function HomeVideoPlayer(element) {
   const caption = element.querySelector('.video-caption');
 
   const handleClick = () => {
-    element.classList.add('splash__video--playing')
+    element.classList.add('splash__video--playing');
   };
 
   element.addEventListener('click', handleClick);
